4135045e635c80a06e19055a0ae6a5611739f50f
aicafiles <aicarandang088@gmail.com>
2025-05-14 19:05:58 +0800

    Remove unused variables

--- Source/Alu.java
+++ Source/Alu.java
@@ -651,7 +651,6 @@ public class Alu extends JFrame {
             public void paintIcon(Component c, Graphics g, int x, int y) {
                 Graphics2D g2 = (Graphics2D) g.create();
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                int w = getIconWidth(), h = getIconHeight();
                 int offsetX = x, offsetY = y;
                 g2.setColor(new Color(180, 0, 0));
                 g2.fillRoundRect(offsetX + 6, offsetY + 10, 16, 14, 6, 6);
